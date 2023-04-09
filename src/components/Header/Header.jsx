a1d5ed1cad5a3f049db4799900f98f20fbf12bf8
aalnoman10 <aalnomanm10@gmail.com>
2023-04-10 05:45:52 +0600

    navber is complate

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css'
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav className='flex nav container'>
+            <div>
+                <Link className='logo a' to='/'>JobHiring</Link>
+            </div>
+            <div className='nav-mid'>
+                <Link className='a' to='/statistics'>Statistics</Link>
+                <Link className='a' to='/applied-Jobs'>Applied Jobs</Link>
+                <Link className='a' to='/blog'>Blog</Link>
+            </div>
+            <div>
+                <Link to='/star-applying'><button className='btn'>Star Applying</button></Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <p>h</p>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
