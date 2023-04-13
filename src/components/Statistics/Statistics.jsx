dbb4d88bc0e7a07b91e6611209ed9729318be65c
aalnoman10 <aalnomanm10@gmail.com>
2023-04-13 20:21:48 +0600

    Statistics page is complate

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Chart } from "react-google-charts";
+
+
+const data = [
+    ["Task", "Hours per Day"],
+    ["Assignment : 1", 57],
+    ["Assignment : 2", 48],
+    ["Assignment : 3", 58],
+    ["Assignment : 4", 53],
+    ["Assignment : 5", 58],
+    ["Assignment : 6", 58],
+    ["Assignment : 7", 60],
+];
+
+const options = {
+    title: `Assignment Analytics`,
+    
+};
 
 const Statistics = () => {
     return (
-        <div>
-            Statistics
-        </div>
+        <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+        />
     );
 };
 
